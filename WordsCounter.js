9d610887148741777ad852146bb7c783bd9f58ca
Arvin Kent S. Lazaga <arvinkent121816@gmail.com>
2020-01-09 13:30:56 +0800

    initial commit

---
+++ WordsCounter.js
@@ -0,0 +1,7 @@
+function countwords(sentence) {
+    return sentence.trim().split(/\s+/).length;
+}
+
+let sentence = 'A be quick brown fox jumps over the lazy dog';
+
+console.log(countwords(sentence))
